1fa28b0537d71db692c5083cd85bdb5f4383ea48
zxzalisa <xinzhi.zhang@yale.edu>
2023-11-30 22:51:16 -0500

    p

--- R/globals.R
+++ R/globals.R
@@ -1,3 +1,3 @@
 utils::globalVariables(c("X", "Y", "Z", "Acceleration", "source_class",
                          "nct_id","brief_title","start_date",
-                         "completion_date","phase","n"，"studies"))
+                         "completion_date"))
