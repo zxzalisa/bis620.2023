7c6817c530baa99b5252f9a0f0943d343bbf3d7b
zxzalisa <xinzhi.zhang@yale.edu>
2023-12-01 22:23:11 -0500

    aaa

--- R/globals.R
+++ R/globals.R
@@ -1,3 +1,4 @@
 utils::globalVariables(c("X", "Y", "Z", "Acceleration", "source_class",
                          "nct_id","brief_title","start_date","as_tibble",
-                         "completion_date","phase","studies","n"))
+                         "completion_date","phase","studies","n","interventions",
+                         "wordcloud","conditions","name"))
